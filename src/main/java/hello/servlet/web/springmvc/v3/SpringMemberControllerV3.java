a473d4375ce04937b11999d2bd02ba71f7677893
jwkcp <jaewoong.go@gmail.com>
2023-02-16 08:17:21 +0900

    스프링 MVC 1편: MVC프레임워크 만들기 - 스프링MVC 실용적인 방식 완료

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -5,7 +5,11 @@ import hello.servlet.domain.member.MemberRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -15,33 +19,43 @@ import java.util.List;
 public class SpringMemberControllerV3 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    @GetMapping("/new-form")
     public String form() {
         return "new-form";
     }
 
-    @RequestMapping("/save")
-    public ModelAndView save(HttpServletRequest req, HttpServletResponse resp) {
-        String username = req.getParameter("username");
-        int age = Integer.parseInt(req.getParameter("age"));
+    @PostMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model
+    ) {
+//        String username = req.getParameter("username");
+//        int age = Integer.parseInt(req.getParameter("age"));
 
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        ModelAndView mv = new ModelAndView("save-result");
-        mv.getModel().put("member", member);
+//        ModelAndView mv = new ModelAndView("save-result");
+//        mv.getModel().put("member", member);
 
-        return mv;
+//        return mv;
+
+        model.addAttribute("member", member);
+        return "save-result";
     }
 
-    @RequestMapping
-    public ModelAndView list() {
+    @GetMapping
+    public String list(Model model) {
         List<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members";
 
-        ModelAndView mv = new ModelAndView("members");
-        mv.addObject("members", members);
 
-        return mv;
+//        ModelAndView mv = new ModelAndView("members");
+//        mv.addObject("members", members);
+//
+//        return mv;
     }
 }
 
